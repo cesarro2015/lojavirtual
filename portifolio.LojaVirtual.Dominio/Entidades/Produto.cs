dadfbc290b4a5e0a9396736deac46fd0ebd0daa0
Cesar Rodrigues <cesaraugustobr2009@hotmail.com>
2015-10-07 21:53:24 -0300

    Classe Produto

---
+++ portifolio.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portifolio.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int PodutoId {get; set;}
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
