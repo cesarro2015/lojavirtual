1b38a278511bf33231d1f0d631d742182e84c7da
Cesar Rodrigues <cesaraugustobr2009@hotmail.com>
2015-10-10 23:46:31 -0300

    Entity

---
+++ portifolio.LojaVirtual.Dominio/Repositorio/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using portifolio.LojaVirtual.Dominio.Entidades;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace portifolio.LojaVirtual.Dominio.Repositorio
+{
+    public class LJDbcontext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ portifolio.LojaVirtual.Dominio/Repositorio/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using portifolio.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portifolio.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutoRepository
+    {
+        private readonly LJDbcontext _context = new LJDbcontext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get {return _context.Produtos; }
+        }
+    }
+}
