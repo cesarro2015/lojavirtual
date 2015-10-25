6df18f933b883c279edcf6c5e0abc863a43f0b41
Cesar Rodrigues <cesaraugustobr2009@hotmail.com>
2015-10-25 21:55:03 -0200

    Add reference

---
+++ portifolio.LojaVirtual.UnitTest/LojaVirtualTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using portifolio.LojaVirtual.Web1.Models;
+using System;
+using System.Web.Mvc;
+
+namespace portifolio.LojaVirtual.UnitTest
+{
+    class LojaVirtualTest
+    {
+        [TestMethod]
+        public void TesteGeracaodePaginacao()
+        {
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                IntensPorPagina = 10,
+                IntensTotal = 28,
+            };
+            Func<int, string> PaginaURL = i => "Pagina" + i;
+
+
+            //act
+            //MvcHtmlString resultado = html.pageLinks(paginacao, PaginaURL);
+
+            //assert
+
+        }
+    }
+}
